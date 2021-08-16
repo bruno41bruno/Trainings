81925bd23becd5a1696052f7109716d16483d71b
felipesouza16 <fesouza2345@gmail.com>
2021-08-16 15:14:41 -0300

    Created Model Schedule Closed #6

---
+++ Trainings/Data/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Schedule : BaseModel
+    {
+        public DateTime Date { get; set; }
+        //public List<Profile> ListProfiles { get; set; }
+        //public List<Content> ListContent { get; set; }
+    }
+}
