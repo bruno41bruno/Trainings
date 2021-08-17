339fe6c9b829bf0a2f40033d05ff8c5512a39bbb
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2021-08-17 17:31:26 -0300

    Criado pasta Repository com BaseRepository Generico Closed #10

---
+++ Trainings/Data/Repository/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using Data.Context;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class BaseRepository<M> where M : BaseModel
+    {
+        public void Create(M model)
+        {
+            using (var context = new BaseContext())
+            {
+                context.Set<M>().Add(model);
+            }
+        }
+
+        public List<M> Read()
+        {
+            using (var context = new BaseContext())
+            {
+                return context.Set<M>().ToList<M>();
+            }
+        }
+
+        public M Read(int id)
+        {
+            using (var context = new BaseContext())
+            {
+                return context.Set<M>().Find(id);
+            }
+        }
+
+        public void Update(M model)
+        {
+            using (var context = new BaseContext())
+            {
+                context.Entry<M>(model).State = EntityState.Modified;
+            }
+        }
+
+        public void Delete(M model)
+        {
+            using (var context = new BaseContext())
+            {
+                context.Entry<M>(model).State = EntityState.Deleted;
+            }
+        }
+    }
+}
