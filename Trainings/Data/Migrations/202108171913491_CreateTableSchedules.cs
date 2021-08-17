3597f518060505f1685db22d8cb4ea2c8ad6a78c
Eduardo Weber <edu.weber07@gmail.com>
2021-08-17 16:17:28 -0300

    Tabelas Schedule e Evaluation criadas | Closed #14

---
+++ Trainings/Data/Migrations/202108171913491_CreateTableSchedules.cs
@@ -0,0 +1,26 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableSchedules : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Schedules",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Schedules");
+        }
+    }
+}
